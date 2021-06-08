d15c9b98a7b910b991f45b00569ca39fe7b166c4
CaoHuy26 <huyc197@gmail.com>
2021-06-09 01:37:40 +0700

    (lession 1) chaper 3: state varibales and integer

--- Lession1.sol
+++ Lession1.sol
@@ -2,5 +2,10 @@ pragma solidity >= 0.5.0 < 0.6.0 //1. Enter solidity version here
 
 //2. Create contract here
 contract ZombieFactory {
-    
+  
+  // State variables will be stored permanently in the blockchain
+  // The uint data type is an unsigned integer (non-negative)
+  // uint is actually an alias for uint256
+  uint dnaDigits = 16;
+  
 }
