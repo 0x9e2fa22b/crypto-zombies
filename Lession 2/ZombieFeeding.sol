11a1f6655fc4908107d6e394aa9344309af0d82c
CaoHuy26 <huyc197@gmail.com>
2021-06-12 00:25:01 +0700

    (lession 2) chaper 7: storage & memory

--- Lession 2/ZombieFeeding.sol
+++ Lession 2/ZombieFeeding.sol
@@ -5,6 +5,10 @@ import './ZombieFactory.sol';
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
+
+    // Most of the time you don't need to use these keywords because Solidity handles them by default.
+    // State variables (variables declared outside of functions) are by default storage and written permanently to the blockchain,
+    // while variables declared inside functions are memory and will disappear when the function call ends.
     Zombie storage myZombie = zombies[_zombieId];
   }
 }
