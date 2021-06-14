b84d1abd7ee8e2d1d80c8dcbf32d9c0b76290fc9
CaoHuy26 <huyc197@gmail.com>
2021-06-14 23:11:14 +0700

    (lession 3) chaper 5: time units

--- Lession 3/ZombieFactory.sol
+++ Lession 3/ZombieFactory.sol
@@ -9,6 +9,8 @@ contract ZombieFactory is Ownable {
   uint dnaDigits = 16;
   uint dnaModulus = 10 ** dnaDigits;
 
+  uint cooldownTime = 1 days;
+
   struct Zombie {
     string name;
     uint dna;
@@ -23,7 +25,14 @@ contract ZombieFactory is Ownable {
   mapping (address => uint) ownerZombieCount;
 
   function _createZombie(string memory _name, uint _dna) internal {
-    uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    uint id = zombies.push(
+      Zombie(
+        _name,
+        _dna,
+        1,
+        uint32(now + cooldownTime)
+      )
+    ) - 1;
 
     zombieToOwner[id] = msg.sender;
     ownerZombieCount[msg.sender]++;
