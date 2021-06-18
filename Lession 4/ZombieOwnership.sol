0947d9fe4fe1c8d5cd1edc28fe74b11a398f2625
CaoHuy26 <huyc197@gmail.com>
2021-06-19 02:45:57 +0700

    (lession 5) chaper 2: implement token erc721

--- Lession 4/ZombieOwnership.sol
+++ Lession 4/ZombieOwnership.sol
@@ -1,10 +1,12 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./ZombieAttack.sol";
+import "./ERC721.sol";
 
 // ERC721 tokens are not interchangeable since each one is assumed to be unique, and are not divisible.
 // You can only trade them in whole units, and each one has a unique ID.
 // So these are a perfect fit for making our zombies tradeable.
-contract ZombieOwnership is ZombieAttack {
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
